e87343b865fbe4bf6a7c05c938c93338bad613ac
gwptoobe@naver.com <toobe@naver.com>
2019-05-03 19:21:06 +0900

    tt

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import Subject from'./components/Subject';
+import Nav from './components/Nav';
+import Content from'./components/Content';
+import { tsConstructorType } from '@babel/types';
+class  App extends Component {
+  constructor(props){
+  super(props);
+    this.state = {
+      mode :'read',
+      Subject:{title:'WEB', sub:'world wide web!'},
+      welcome :{title:'welcome', desc:'hello , React!'},
+      contents:[
+        {id:1, title:'html', desc:"인포메이션"},
+        {id:2, title:'css', desc:"디자인"},
+        {id:3, title:'javascript', desc:"자바스크립트"}
+      ]
+    }
+}
+render(){
+  var _title, _desc = null;
+  if(this.state.mode ==='welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+  }else if (this.state.mode === 'read'){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Subject title={this.state.Subject.title} sub={this.state.Subject.sub}></Subject>
+      <Subject title='e2' sub='sub2'></Subject>
+      <Nav data={this.state.contents}></Nav>
+      <Content title={_title} desc={_desc}></Content>
     </div>
   );
 }
-
+}
 export default App;
+
+
+
+
